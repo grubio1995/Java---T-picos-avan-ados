51f145b92db86d55a7dbcd8a421ca6ad1e69d251
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-10-06 23:12:46 -0300

    Java 8 - Exemplo Method reference - Construtor

--- Java 8/Method references/Exemplo01/src/Exemplo01.java
+++ Java 8/Method references/Exemplo01/src/Exemplo01.java
@@ -1,9 +1,9 @@
 interface Figura2d {
-    void desenha(Double largura, Double altura);
+    Retangulo desenha(Double largura, Double altura);
 }
 
 class Retangulo {
-    public void desenhaRetangulo(Double largura, Double altura) {
+    public Retangulo(Double largura, Double altura) {
         System.out.println("Desenha retângulo de Largura: " + largura + " e Altura: " + altura);
     }
 }
@@ -13,14 +13,7 @@ public class Exemplo01 {
 
     public static void main(String[] args) {
 
-        // Lambda expression
-      Figura2d fig1 = (largura, altura) -> System.out.println("Desenha figura de Largura: " + largura + " e Altura: " + altura);
-      fig1.desenha(8.0d,1.5d);
-
-      //Objeto específico
-      Retangulo ret = new Retangulo();
-      //Method Reference
-      Figura2d fig2 = ret :: desenhaRetangulo;
-      fig2.desenha(10.5 ,7.0);
+        Figura2d fig1 = Retangulo :: new;
+        fig1.desenha(10.0,2.5);
     }
 }
