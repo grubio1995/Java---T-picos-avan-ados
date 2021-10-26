033279038222bf18394e8b589dd7f09a835eacaf
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-10-26 20:43:40 -0300

    Java 9 - http2

---
+++ Java 9/Http2/src/HTTP2Exemplo.java
@@ -0,0 +1,13 @@
+
+import java.net.http.HttpClient;
+
+public class HTTP2Exemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HttpClient client =  HttpClient.newHttpClient();
+
+	}
+
+}
